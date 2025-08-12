0a239f19df3beb5c8a0855fd47b852b302cb0b5e
Manikandan <r.manikandan3112@gmail.com>
2025-08-12 15:19:10 +0530

    Add files via upload

---
+++ evaluationtask/app.py
@@ -0,0 +1,30 @@
+import streamlit as st
+from utils.recommand import get_popular_destinations, filter_hotels
+import pandas as pd
+
+@st.cache_data
+def load_data():
+    return pd.read_csv('/home/bharathi/Documents/vitetech/evalutiontask/data/hotel_dataset_200_cleaned.csv')
+
+df = load_data()
+
+st.title("🏨 Hotel Search System")
+
+user_input = st.text_input("💬 Enter your preferences (e.g., location, price, amenities):")
+
+# Button
+if st.button("🔍 Get Recommendations"):
+    if user_input:
+        recommendations = filter_hotels(df, user_input)
+        if recommendations is not None and not recommendations.empty:
+            st.success("✨ Recommended Hotels for You:")
+            st.dataframe(recommendations)
+        else:
+            st.warning("⚠️ No hotels found matching the criteria.")
+            st.info("🌍 Here are some popular destinations instead:")
+            popular_destinations = get_popular_destinations(df)
+            st.dataframe(popular_destinations)
+    else:
+        st.warning("✏️ Please enter your preferences to get recommendations.")
+else:
+    st.write("ℹ️ Enter your preferences above and click the button to see hotel recommendations.")
